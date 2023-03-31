54b49d96a189ec5f2d0ba9bd0ba0e0bf194a304b
BohdanLytvynov <blitvinov19@gmail.com>
2023-03-31 14:45:45 +0300

    Initial state

---
+++ Notes_Web_Server/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Notes_Web_Server.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
